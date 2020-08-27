009b4673e952470e5ca81ec2a71f943c97f4f85f
Delower <delower.hosen00@gmail.com>
2020-08-27 15:34:45 +0600

    first docker image

---
+++ simple-web/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = new express();
+
+app.get('/', (req, res) => {
+    res.send('Hi there');
+});
+
+app.listen(8080, () => {
+    console.log('listening on port 8080');
+});
